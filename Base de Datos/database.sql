ea1fd28d776f04d05731abe8496d113baf619787
fedegy <fede88662@gmail.com>
2021-03-28 19:47:49 -0600

    Carpetas Frontend y Base de Datos

---
+++ Base de Datos/database.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE PORTAL_ESTUDIANTIL;
+USE PORTAL_ESTUDIANTIL;
+
+CREATE TABLE registro(
+	carnet INT NOT NULL,
+	nombres VARCHAR(50) NOT NULL,
+	apellidos VARCHAR(50) NOT NULL,
+	contrasena VARCHAR(50) NOT NULL,
+	correo VARCHAR(50) NOT NULL,
+	PRIMARY KEY (carnet)
+);
+
+
+SELECT*FROM registro;
+
+
+
+
