54bed9c83cdd940b1c8b8cdb67ea690bc8ae089a
fedegy <fede88662@gmail.com>
2021-03-30 22:51:38 -0600

    Se levanto el servidor

--- Backend/server/src/index.js
+++ Backend/server/src/index.js
@@ -1,23 +1,35 @@
+//Se utilizan los siguientes modulos para su posterior uso
+/*Uso de express como framework y mysql para las funciones necesarias
+de la base de datos 
+Se utiliza body parser para tener acceso a body y para hacer uso de post
+*/
+const bodyP = require('body-parser');
 const exp=require('express')
 const mysql=require('mysql')
 const app=exp()
 
+//Se creo conexion de base de datos
 const db=mysql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"portal_estudiantil"
+    host:"localhost",               //Host de alojamiento de base de datos
+    user:"root",                    //Usuario de host
+    password:"",                    //Contraseña
+    database:"portal_estudiantil"   //Nombre de la base de datos
 });
 
+//Ruta por defecto de servidor
 app.get('/',(req,res)=>{
-    const insertar_tabla="INSERT INTO registro (carnet,nombres,apellidos,contrasena,correo) VALUES (202000000,'Usuario 1','User','12345','correo@example.com');"
-    db.query(insertar_tabla,(err,result)=>{
-        res.send('Servidor Node Js')
-    })
-    
+    console.log("Servidor Node JS")
+    res.send("Servidor Node JS")
 });
 
+//Haciendo uso de body-parser
+app.use(bodyP.urlencoded({
+    extended:true
+}));
+
+
+//Puerto en donde sera ejecutado
 app.listen(3001,()=>{
-    console.log('Servidor en puerto 3001')
+    console.log('Servidor corriendo')
 });
 
