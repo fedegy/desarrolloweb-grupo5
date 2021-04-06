e17404c4ac1a81bf81d6c1b45a5187d3d6235c88
fedegy <fede88662@gmail.com>
2021-04-06 17:29:52 -0600

    Se agregaron tablas publicacion y cursos aprobados

--- Base de Datos/database_prueba.sql
+++ Base de Datos/database_prueba.sql
@@ -14,3 +14,32 @@ CREATE TABLE registro(
 /*Seleccionar valores de tabla registro*/
 SELECT*FROM registro;
 
+CREATE TABLE publicacion(
+	id INT NOT NULL AUTO_INCREMENT,
+    carnet INT NOT NULL,
+    sujeto VARCHAR(50) NOT NULL,
+    mensaje VARCHAR(510) NOT NULL,
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    tipo INT NOT NULL,
+    PRIMARY KEY(id)
+    
+);
+
+SELECT*FROM publicacion;
+
+
+CREATE TABLE cursos_aprobados_dtt(
+	id_curso INT NOT NULL,
+    carnet INT NOT NULL,
+    PRIMARY KEY(id_curso)
+);
+
+SELECT*FROM cursos_aprobados_dtt;
+
+
+CREATE TABLE cursos(
+	id_curso INT NOT NULL,
+    nombre_curso VARCHAR(100) NOT NULL,
+    creditos INT NOT NULL,
+    PRIMARY KEY (id_curso)
+);
