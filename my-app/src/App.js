c623cf22eefbbf5248b265d8c8832b7d221fe04f
daraBurst <monzon_rafael@hotmail.com>
2021-04-06 19:57:42 -0600

    routes fixed and password reset pasge added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,28 @@
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
+import Password from "./Password";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/">
-          <SignIn />
-        </Route>
-      </Switch>
-
-      <Switch>
-        <Route path="/signup">
-          <SignUp />
-        </Route>
-      </Switch>
+      <div className="container">
+        <Switch>
+          <Route exact path="/signup">
+            <SignUp />
+          </Route>
+        </Switch>
+        <Switch>
+          <Route exact path="/reset">
+            <Password />
+          </Route>
+        </Switch>
+        <Switch>
+          <Route exact path="/">
+            <SignIn />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
--- my-app/src/SignUp.js
+++ my-app/src/SignUp.js
@@ -1,42 +1,28 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(3),
   },
   submit: {
@@ -87,6 +73,17 @@ export default function SignUp() {
                 variant="outlined"
                 required
                 fullWidth
+                id="studentId"
+                label="Student Id"
+                name="studentId"
+                autoComplete="sid"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
                 id="email"
                 label="Email Address"
                 name="email"
@@ -105,7 +102,6 @@ export default function SignUp() {
                 autoComplete="current-password"
               />
             </Grid>
-
           </Grid>
           <Button
             type="submit"
@@ -118,16 +114,13 @@ export default function SignUp() {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="./SignIn" variant="body2">
+              <Link href="./" variant="body2">
                 Already have an account? Sign in
               </Link>
             </Grid>
           </Grid>
         </form>
       </div>
-      <Box mt={5}>
-        <Copyright />
-      </Box>
     </Container>
   );
 }
