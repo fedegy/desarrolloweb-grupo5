456b4182ad91a639af817d2e34740b5cbb3ed0b5
Serch-ING <3000523830101@ingenieria.usac.edu.gt>
2021-04-09 21:26:54 -0600

    Actulizacion y enlaces comntarios y publicacion

--- my-app/src/components/DropPerfil.js
+++ my-app/src/components/DropPerfil.js
@@ -2,6 +2,7 @@
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
+import axios from 'axios'
 
 class DropPerfil extends Component {
   state = {
--- my-app/src/components/DropProfesores.js
+++ my-app/src/components/DropProfesores.js
@@ -1,7 +1,7 @@
 // import axios from 'axios';
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
-
+import axios from 'axios'
 class DropProfesores extends Component {
    state={
        cate:'Mario',
